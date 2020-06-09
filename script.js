333c6511ccc24fee2cd5035116b0fccb6184c1ac
l4legenda <l4legenda@gmail.com>
2020-06-10 00:22:13 +0500

    game

---
+++ script.js
@@ -0,0 +1,42 @@
+let vue = new Vue({
+  el: '#app',
+  data: {
+    app : [
+    	{isActive : false},
+    	{isActive : false},
+    	{isActive : false},
+    	{isActive : false},
+    	{isActive : false},
+    	{isActive : false},
+    	{isActive : false},
+    	{isActive : false},
+    	{isActive : false}
+    ]
+  },
+  methods: {
+  	cl(id){
+  		this.app[id].isActive = true;
+  		  var audio = new Audio(); // Создаём новый элемент Audio
+		  audio.src = 'sound/sound'+ (id + 1) +'.mp3'; // Указываем путь к звуку "клика"
+		  audio.volume = 0.2;
+		  audio.play(); // Автоматически запускаем
+		  document.querySelector("#timer").classList.add("a");
+		  setTimeout(() => {
+		  	let value = 0.2;
+		  	let i = setInterval(()=>{
+		  		value -= 0.02;
+		  		audio.volume = value;
+		  	}, 100);
+		  	setTimeout(()=>{
+		  		clearInterval(i);
+		  		audio.pause();
+		  		document.querySelector("#timer").classList.remove("a");
+		  	}, 1000);
+		  	
+		  	
+		  }, 14000);
+  	}
+  }
+});
+
+
