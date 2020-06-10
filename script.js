ac87904cbb5c2fe38396b7ab17a9686783109ff6
Your Name <you@example.com>
2020-06-10 11:03:51 +0500

    game2

--- script.js
+++ script.js
@@ -2,22 +2,41 @@ let vue = new Vue({
   el: '#app',
   data: {
     app : [
-    	{isActive : false},
-    	{isActive : false},
-    	{isActive : false},
-    	{isActive : false},
-    	{isActive : false},
-    	{isActive : false},
-    	{isActive : false},
-    	{isActive : false},
-    	{isActive : false}
+      {name : "list1", item: [
+        {isActive : false},
+        {isActive : false},
+        {isActive : false},
+        {isActive : false},
+        {isActive : false}
+      ]},
+    	{name : "list2", item: [
+        {isActive : false},
+        {isActive : false},
+        {isActive : false},
+        {isActive : false},
+        {isActive : false}
+      ]},
+      {name : "list3", item: [
+        {isActive : false},
+        {isActive : false},
+        {isActive : false},
+        {isActive : false},
+        {isActive : false}
+      ]},
+      {name : "list4", item: [
+        {isActive : false},
+        {isActive : false},
+        {isActive : false},
+        {isActive : false},
+        {isActive : false}
+      ]}
     ]
   },
   methods: {
-  	cl(id){
-  		this.app[id].isActive = true;
+  	cl(id, id2){
+  		this.app[id].item[id2].isActive = true;
   		  var audio = new Audio(); // Создаём новый элемент Audio
-		  audio.src = 'sound/sound'+ (id + 1) +'.mp3'; // Указываем путь к звуку "клика"
+		  audio.src = 'sound/' + 'list' + (id + 1) + '/sound'+ (id + 1) +'.mp3'; // Указываем путь к звуку "клика"
 		  audio.volume = 0.2;
 		  audio.play(); // Автоматически запускаем
 		  document.querySelector("#timer").classList.add("a");
